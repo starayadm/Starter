63594ebd55fab9cb0b6233af5611c6641398e01e
Lily <33253432+starayadm@users.noreply.github.com>
2019-11-15 21:04:15 +0300

    First commit

---
+++ Starter.v.1/gulpfile.js
@@ -0,0 +1,117 @@
+var gulp         = require('gulp'),
+		sass         = require('gulp-sass'),
+		browserSync  = require('browser-sync'),
+		concat       = require('gulp-concat'),
+		uglify       = require('gulp-uglify-es').default,
+		cleancss     = require('gulp-clean-css'),
+		autoprefixer = require('gulp-autoprefixer'),
+		rsync        = require('gulp-rsync'),
+		newer        = require('gulp-newer'),
+		rename       = require('gulp-rename'),
+		responsive   = require('gulp-responsive'),
+		svgSprite 	 = require('gulp-svg-sprite'),
+		cheerio 		 = require('gulp-cheerio'),
+		replace 		 = require('gulp-replace'),
+		del          = require('del');
+
+// Local Server
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// online: false, // Work offline without internet connection
+		// tunnel: true, tunnel: 'projectname', // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+function bsReload(done) { browserSync.reload(); done(); };
+
+// Custom Styles
+gulp.task('styles', function() {
+	return gulp.src('app/sass/**/*.sass')
+	.pipe(sass({
+		outputStyle: 'expanded',
+		includePaths: [__dirname + '/node_modules']
+	}))
+	.pipe(concat('styles.min.css'))
+	.pipe(autoprefixer({
+		grid: true,
+		overrideBrowserslist: ['last 10 versions']
+	}))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream())
+});
+
+// Scripts & JS Libraries
+gulp.task('scripts', function() {
+	return gulp.src([
+		// 'node_modules/jquery/dist/jquery.min.js', // Optional jQuery plug-in (npm i --save-dev jquery)
+		'app/js/_custom.js', // Custom scripts. Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify()) // Minify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+// Clean @*x IMG's
+gulp.task('cleanimg', function() {
+	return del(['app/img/@*'], { force: true })
+});
+
+// Code & Reload
+gulp.task('code', function() {
+	return gulp.src('app/**/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+// Deploy
+gulp.task('rsync', function() {
+	return gulp.src('app/')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Included files
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excluded files
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+// Svg-Sprite Creating
+gulp.task('svg-use', function () {
+	return gulp.src('app/img/svg/*.svg')
+	// remove all fill, style and stroke declarations in out shapes
+	.pipe(cheerio({
+		run: function ($) {
+			$('[fill]').removeAttr('fill');
+			$('[stroke]').removeAttr('stroke');
+			$('[style]').removeAttr('style');
+		},
+		parserOptions: {xmlMode: true}
+	}))
+	// cheerio plugin create unnecessary string '&gt;', so replace it.
+	.pipe(replace('&gt;', '>'))
+	// build svg sprite
+	.pipe(svgSprite({
+		mode: {
+			symbol: {
+						dest:'.'
+		}
+	}
+	}))
+	.pipe(gulp.dest('app/img/svg-sprite'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('app/sass/**/*.sass', gulp.parallel('styles'));
+	gulp.watch(['app/js/_custom.js', 'app/js/_libs.js'], gulp.parallel('scripts'));
+	gulp.watch('app/*.html', gulp.parallel('code'));
+});
+
+gulp.task('default', gulp.parallel('styles', 'scripts', 'browser-sync', 'watch'));
